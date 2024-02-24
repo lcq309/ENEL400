7988995efcf43323581186e9475ab8f44019e1b5
mwk2000 <MWK779@uregina.ca>
2024-02-24 15:02:42 -0600

    added LED blink
    This should also now blink an LED

--- Software/Startup testing code/MPLABproject/Startup testing 2.X/main.c
+++ Software/Startup testing code/MPLABproject/Startup testing 2.X/main.c
@@ -5,10 +5,12 @@
  * Created on February 24, 2024, 1:12 PM
  */
 
+#define F_CPU 24000000UL //24MHz clock for delay function
+
 #include <stdio.h>
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdlib.h>
-
 
 /*
  * 
@@ -33,7 +35,24 @@ int main(int argc, char** argv) {
     /*
      * That should be all the clock configuration required, now I will try to
      * configure an I/O pin as an output to flash an LED.
+     * it seems that the peripheral clocks happen automatically or something,
+     * I do not appear to need to enable them like I do with the STM32
+     * to enable a pin as output, the header path seems to be PORTx.DIRSET
+     * I will then use the pin 3 bit mask to set that pin as an output
      */
+    PORTD.DIRSET |= PIN3_bm;
+    /*
+     * now PIN3 should be configured as an output, so I will try toggling it
+     * once per second
+     */
+    while(1)
+    {
+        PORTD.OUTSET |= PIN3_bm;
+        _delay_ms(1000);
+        PORTD.OUTCLR |= PIN3_bm;
+        _delay_ms(1000);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
