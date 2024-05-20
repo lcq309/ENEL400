47915ec53a98f24df07932d7704b75d6c019d487
mwk2000 <MWK779@uregina.ca>
2024-05-20 16:11:32 -0600

    Revert "Work on sector light DSIO"
    This reverts commit 22fd5f0585ef72d2bedc979acd46b6e94d4aaae3.

--- Production Software/Devices/485SecCont.X/nbproject/Makefile-default.mk
+++ Production Software/Devices/485SecCont.X/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../../Drivers/USART.c ../../Drivers/ShiftReg.c ../../FreeRTOS/AVR_AVRDx/port.c ../../FreeRTOS/event_groups.c ../../FreeRTOS/heap_1.c ../../FreeRTOS/list.c ../../FreeRTOS/queue.c ../../FreeRTOS/stream_buffer.c ../../FreeRTOS/tasks.c ../../FreeRTOS/timers.c main.c ../../ModularTasks/Communications/RS485TASKS.c ../../Drivers/DSIO.c
+SOURCEFILES_QUOTED_IF_SPACED=../../Drivers/USART.c ../../Drivers/ShiftReg.c ../../FreeRTOS/AVR_AVRDx/port.c ../../FreeRTOS/event_groups.c ../../FreeRTOS/heap_1.c ../../FreeRTOS/list.c ../../FreeRTOS/queue.c ../../FreeRTOS/stream_buffer.c ../../FreeRTOS/tasks.c ../../FreeRTOS/timers.c ../../ModularTasks/Communications/RS485TASKS.c main.c ../../Drivers/DSIO.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/83377035/USART.o ${OBJECTDIR}/_ext/83377035/ShiftReg.o ${OBJECTDIR}/_ext/1347852338/port.o ${OBJECTDIR}/_ext/1644529106/event_groups.o ${OBJECTDIR}/_ext/1644529106/heap_1.o ${OBJECTDIR}/_ext/1644529106/list.o ${OBJECTDIR}/_ext/1644529106/queue.o ${OBJECTDIR}/_ext/1644529106/stream_buffer.o ${OBJECTDIR}/_ext/1644529106/tasks.o ${OBJECTDIR}/_ext/1644529106/timers.o ${OBJECTDIR}/main.o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ${OBJECTDIR}/_ext/83377035/DSIO.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/83377035/USART.o.d ${OBJECTDIR}/_ext/83377035/ShiftReg.o.d ${OBJECTDIR}/_ext/1347852338/port.o.d ${OBJECTDIR}/_ext/1644529106/event_groups.o.d ${OBJECTDIR}/_ext/1644529106/heap_1.o.d ${OBJECTDIR}/_ext/1644529106/list.o.d ${OBJECTDIR}/_ext/1644529106/queue.o.d ${OBJECTDIR}/_ext/1644529106/stream_buffer.o.d ${OBJECTDIR}/_ext/1644529106/tasks.o.d ${OBJECTDIR}/_ext/1644529106/timers.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d ${OBJECTDIR}/_ext/83377035/DSIO.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/83377035/USART.o ${OBJECTDIR}/_ext/83377035/ShiftReg.o ${OBJECTDIR}/_ext/1347852338/port.o ${OBJECTDIR}/_ext/1644529106/event_groups.o ${OBJECTDIR}/_ext/1644529106/heap_1.o ${OBJECTDIR}/_ext/1644529106/list.o ${OBJECTDIR}/_ext/1644529106/queue.o ${OBJECTDIR}/_ext/1644529106/stream_buffer.o ${OBJECTDIR}/_ext/1644529106/tasks.o ${OBJECTDIR}/_ext/1644529106/timers.o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ${OBJECTDIR}/main.o ${OBJECTDIR}/_ext/83377035/DSIO.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/83377035/USART.o.d ${OBJECTDIR}/_ext/83377035/ShiftReg.o.d ${OBJECTDIR}/_ext/1347852338/port.o.d ${OBJECTDIR}/_ext/1644529106/event_groups.o.d ${OBJECTDIR}/_ext/1644529106/heap_1.o.d ${OBJECTDIR}/_ext/1644529106/list.o.d ${OBJECTDIR}/_ext/1644529106/queue.o.d ${OBJECTDIR}/_ext/1644529106/stream_buffer.o.d ${OBJECTDIR}/_ext/1644529106/tasks.o.d ${OBJECTDIR}/_ext/1644529106/timers.o.d ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/_ext/83377035/DSIO.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/83377035/USART.o ${OBJECTDIR}/_ext/83377035/ShiftReg.o ${OBJECTDIR}/_ext/1347852338/port.o ${OBJECTDIR}/_ext/1644529106/event_groups.o ${OBJECTDIR}/_ext/1644529106/heap_1.o ${OBJECTDIR}/_ext/1644529106/list.o ${OBJECTDIR}/_ext/1644529106/queue.o ${OBJECTDIR}/_ext/1644529106/stream_buffer.o ${OBJECTDIR}/_ext/1644529106/tasks.o ${OBJECTDIR}/_ext/1644529106/timers.o ${OBJECTDIR}/main.o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ${OBJECTDIR}/_ext/83377035/DSIO.o
+OBJECTFILES=${OBJECTDIR}/_ext/83377035/USART.o ${OBJECTDIR}/_ext/83377035/ShiftReg.o ${OBJECTDIR}/_ext/1347852338/port.o ${OBJECTDIR}/_ext/1644529106/event_groups.o ${OBJECTDIR}/_ext/1644529106/heap_1.o ${OBJECTDIR}/_ext/1644529106/list.o ${OBJECTDIR}/_ext/1644529106/queue.o ${OBJECTDIR}/_ext/1644529106/stream_buffer.o ${OBJECTDIR}/_ext/1644529106/tasks.o ${OBJECTDIR}/_ext/1644529106/timers.o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ${OBJECTDIR}/main.o ${OBJECTDIR}/_ext/83377035/DSIO.o
 
 # Source Files
-SOURCEFILES=../../Drivers/USART.c ../../Drivers/ShiftReg.c ../../FreeRTOS/AVR_AVRDx/port.c ../../FreeRTOS/event_groups.c ../../FreeRTOS/heap_1.c ../../FreeRTOS/list.c ../../FreeRTOS/queue.c ../../FreeRTOS/stream_buffer.c ../../FreeRTOS/tasks.c ../../FreeRTOS/timers.c main.c ../../ModularTasks/Communications/RS485TASKS.c ../../Drivers/DSIO.c
+SOURCEFILES=../../Drivers/USART.c ../../Drivers/ShiftReg.c ../../FreeRTOS/AVR_AVRDx/port.c ../../FreeRTOS/event_groups.c ../../FreeRTOS/heap_1.c ../../FreeRTOS/list.c ../../FreeRTOS/queue.c ../../FreeRTOS/stream_buffer.c ../../FreeRTOS/tasks.c ../../FreeRTOS/timers.c ../../ModularTasks/Communications/RS485TASKS.c main.c ../../Drivers/DSIO.c
 
 
 
@@ -148,17 +148,17 @@ ${OBJECTDIR}/_ext/1644529106/timers.o: ../../FreeRTOS/timers.c  .generated_files
 	@${RM} ${OBJECTDIR}/_ext/1644529106/timers.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG  -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/_ext/1644529106/timers.o.d" -MT "${OBJECTDIR}/_ext/1644529106/timers.o.d" -MT ${OBJECTDIR}/_ext/1644529106/timers.o -o ${OBJECTDIR}/_ext/1644529106/timers.o ../../FreeRTOS/timers.c 
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/78f921586625db8aebd5a3ddf7465553e4d8e036 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/main.o.d 
-	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG  -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/main.o.d" -MT "${OBJECTDIR}/main.o.d" -MT ${OBJECTDIR}/main.o -o ${OBJECTDIR}/main.o main.c 
-	
 ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o: ../../ModularTasks/Communications/RS485TASKS.c  .generated_files/flags/default/f7ef2e82366195e938d18da58305a93eaed826f1 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}/_ext/2083278984" 
 	@${RM} ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d 
 	@${RM} ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG  -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d" -MT "${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d" -MT ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o -o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ../../ModularTasks/Communications/RS485TASKS.c 
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/78f921586625db8aebd5a3ddf7465553e4d8e036 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG  -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/main.o.d" -MT "${OBJECTDIR}/main.o.d" -MT ${OBJECTDIR}/main.o -o ${OBJECTDIR}/main.o main.c 
 	
 ${OBJECTDIR}/_ext/83377035/DSIO.o: ../../Drivers/DSIO.c  .generated_files/flags/default/3846477d265e0d60bf68c790b97431cce8230c13 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}/_ext/83377035" 
@@ -227,17 +227,17 @@ ${OBJECTDIR}/_ext/1644529106/timers.o: ../../FreeRTOS/timers.c  .generated_files
 	@${RM} ${OBJECTDIR}/_ext/1644529106/timers.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/_ext/1644529106/timers.o.d" -MT "${OBJECTDIR}/_ext/1644529106/timers.o.d" -MT ${OBJECTDIR}/_ext/1644529106/timers.o -o ${OBJECTDIR}/_ext/1644529106/timers.o ../../FreeRTOS/timers.c 
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/f022d288e945db23b9f79eac89a009f5dfd7455b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/main.o.d 
-	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/main.o.d" -MT "${OBJECTDIR}/main.o.d" -MT ${OBJECTDIR}/main.o -o ${OBJECTDIR}/main.o main.c 
-	
 ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o: ../../ModularTasks/Communications/RS485TASKS.c  .generated_files/flags/default/185518f87d4516a10c018241acdcdc07425f724b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}/_ext/2083278984" 
 	@${RM} ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d 
 	@${RM} ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d" -MT "${OBJECTDIR}/_ext/2083278984/RS485TASKS.o.d" -MT ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o -o ${OBJECTDIR}/_ext/2083278984/RS485TASKS.o ../../ModularTasks/Communications/RS485TASKS.c 
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/f022d288e945db23b9f79eac89a009f5dfd7455b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -fno-common -funsigned-char -funsigned-bitfields -I"../../Drivers" -I"../../FreeRTOS/include" -I"FreeRTOSConfig" -I"../../FreeRTOS/AVR_AVRDx" -I"../../ModularTasks/Communications" -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3 -mconst-data-in-progmem -mno-const-data-in-config-mapped-progmem     -MD -MP -MF "${OBJECTDIR}/main.o.d" -MT "${OBJECTDIR}/main.o.d" -MT ${OBJECTDIR}/main.o -o ${OBJECTDIR}/main.o main.c 
 	
 ${OBJECTDIR}/_ext/83377035/DSIO.o: ../../Drivers/DSIO.c  .generated_files/flags/default/f4bde55f10ae312b5f5cba48cc1baa0fe7740418 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}/_ext/83377035" 
---
+++ Production Software/Devices/485SecLight.X/DSIO.c
@@ -0,0 +1,275 @@
+/* 
+ * File:   DSIO.c
+ * Author: Michael King
+ * c file that includes the Device specific I/O tasks
+ * as well as functions that are defined for modular tasks that interact with hardware
+ * Created on April 23, 2024
+ */
+
+#include "DSIO.h"
+//timer globals
+    
+    uint8_t xINDTimerSet;
+    
+    //timer handles
+    
+    TimerHandle_t xINDTimer;
+    
+    //queue handles
+
+    QueueHandle_t xIND_Queue;
+    QueueHandle_t xDeviceIN_Queue;
+    
+void DSIOSetup()
+{
+    //485 R/W pin setup
+    PORTA.DIRSET = PIN2_bm;
+    
+    //setup Queues
+    
+    xIND_Queue = xQueueCreate(3, 2 * sizeof(uint8_t)); // up to 3 Indicator Commands held
+    xDeviceIN_Queue = xQueueCreate(3, 2 * sizeof(uint8_t)); // intertask messages to the device specific task
+    
+    //setup tasks
+    
+    xTaskCreate(dsIOOutTask, "INDOUT", 250, NULL, mainINDOUT_TASK_PRIORITY, NULL);
+    
+    //setup timers
+
+    xINDTimer = xTimerCreate("INDT", 50, pdTRUE, 0, vINDTimerFunc);
+    
+    //start the indicator timer
+    xTimerStart(xINDTimer, 0);
+}
+
+void dsIOOutTask (void * parameters)
+{
+    //blink, flash, solid, etc.
+    //this controls the three indicators on the console
+    //blink should be somewhat slow, flash should be faster (mostly used for blue, initialization, and maybe errors)
+    
+    //indicator state buffers initialized to zero
+    uint8_t GREEN = 0; uint8_t YELLOW = 0; uint8_t BLUE = 0; uint8_t RED = 0;
+    
+    //time buffers for flash and latches
+    uint8_t flash = 0; uint8_t blink = 0; uint8_t ms250 = 0;
+    
+    //setup IO
+    
+    PORTC.DIRSET = PIN2_bm | PIN3_bm;
+    PORTD.DIRSET = PIN0_bm | PIN1_bm; 
+    
+    //receiver buffer
+    uint8_t received[2];
+    
+    //running loop
+    for(;;)
+    {
+        //first, check for commands from other tasks (hold for 50ms)
+        if(xQueueReceive(xIND_Queue, received, 25) == pdTRUE)
+        {
+        switch(received[0]) //first check the intended target
+        {
+            case 0x0: //do nothing/no target
+                break;
+            case 'G': //Green indicator
+                GREEN = received[1];
+                break;
+            case 'Y': //Yellow indicator
+                YELLOW = received[1];
+                break;
+            case 'B': //Blue indicator
+                BLUE = received[1];
+                break;
+            case 'R': //Red indicator
+                RED = received[1];
+                break;
+            case 0xff: //All indicators
+                GREEN = received[1];
+                YELLOW = received[1];
+                BLUE = received[1];
+                break;
+        }
+        }
+        //if timer has triggered, increment ms250 and reset flash and blink timers
+        if(xINDTimerSet == 1)
+        {
+            ms250++;
+            if(ms250 == 5)
+            {
+                ms250 = 0;
+                blink = 0;
+            }
+            flash = 0;
+            xINDTimerSet = 0;
+        }
+        //process green first
+        switch(GREEN)
+        {
+            case 'B': //blink
+                if(blink == 0 && ms250 == 0)
+                    dsioGreenTGL();
+            break;
+            
+            case 'F': //flash
+                if(flash == 0)
+                    dsioGreenTGL();
+            break;
+            
+            case 'S': //solid
+                dsioGreenOn();
+            break;
+            
+            case 'O': //off
+                dsioGreenOff();
+            break;
+        }
+        //process blue
+        switch(BLUE)
+        {
+            case 'B': //blink
+                if(blink == 0 && ms250 == 0)
+                    dsioBlueTGL();
+            break;
+            
+            case 'F': //flash
+                if(flash == 0)
+                    dsioBlueTGL();
+            break;
+            
+            case 'S': //solid
+                dsioBlueOn();
+            break;
+            
+            case 'O': //off
+                dsioBlueOff();
+            break;
+        }
+        //process yellow
+        switch(YELLOW)
+        {
+            case 'B': //blink
+                if(blink == 0 && ms250 == 0)
+                    dsioYellowTGL();
+            break;
+            
+            case 'F': //flash
+                if(flash == 0)
+                    dsioYellowTGL();
+            break;
+            
+            case 'S': //solid
+                dsioYellowOn();
+            break;
+            
+            case 'O': //off
+                dsioYellowOff();
+            break;
+        }
+        //process blue
+        switch(RED)
+        {
+            case 'B': //blink
+                if(blink == 0 && ms250 == 0)
+                    dsioRedTGL();
+            break;
+            
+            case 'F': //flash
+                if(flash == 0)
+                    dsioRedTGL();
+            break;
+            
+            case 'S': //solid
+                dsioRedOn();
+            break;
+            
+            case 'O': //off
+                dsioRedOff();
+            break;
+        }
+        //set blink and flash
+        blink = 1;
+        flash = 1;
+    }
+}
+
+//RS485 in/out
+void RS485TR(uint8_t dir)
+{
+    //set transceiver direction
+    switch(dir)
+    {
+        case 'T': //transmit
+            PORTA.DIRSET = PIN2_bm;
+            break;
+        case 'R': //receive
+            PORTA.DIRCLR = PIN2_bm;
+            break;
+        default: //incorrect command
+            break;
+    }
+}
+
+//timer callback functions
+void vPBTimerFunc( TimerHandle_t xTimer )
+{
+    xPBTimerSet = 1;
+}
+
+void vINDTimerFunc( TimerHandle_t xTimer )
+{
+    xINDTimerSet = 1;
+}
+
+void dsioGreenOn(void)
+{
+    PORTA.OUTSET = PIN7_bm;
+}
+void dsioGreenOff(void)
+{
+    PORTA.OUTCLR = PIN7_bm;
+}
+void dsioGreenTGL(void)
+{
+    PORTA.OUTTGL = PIN7_bm;
+}
+    
+void dsioYellowOn(void)
+{
+    PORTC.OUTSET = PIN0_bm;
+}
+void dsioYellowOff(void)
+{
+    PORTC.OUTCLR = PIN0_bm;
+}
+void dsioYellowTGL(void)
+{
+    PORTC.OUTTGL = PIN0_bm;
+}
+    
+void dsioBlueOn(void)
+{
+    PORTC.OUTSET = PIN1_bm;
+}
+void dsioBlueOff(void)
+{
+    PORTC.OUTCLR = PIN1_bm;
+}
+void dsioBlueTGL(void)
+{
+    PORTC.OUTTGL = PIN1_bm;
+}
+
+void dsioRedOn(void)
+{
+    
+}
+void dsioRedOff(void)
+{
+    
+}
+void dsioRedTGL(void)
+{
+    
+}
+//interrupts go here
