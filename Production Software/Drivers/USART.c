e72b5b88a6959371d8330570c622dc159993aaa8
mwk2000 <MWK779@uregina.ca>
2024-07-10 13:25:32 -0600

    USART drivers updated
    added drivers for USART1 and USART2

--- Production Software/Drivers/USART.c
+++ Production Software/Drivers/USART.c
@@ -28,3 +28,43 @@ void USART0_init(void)
     SREG |= ~CPU_I_bm;
     //USART0 now initialized for 8bit, even parity, interrupt driven mode
 }
+
+void USART1_init(void)
+{
+    //start by disabling global interrupts (if they are enabled)
+    SREG &= ~CPU_I_bm; //set interrupt enable to zero to disable global interrupts
+    //Calculate baud rate
+    USART1.BAUD = ((64 * (long int)CPU_F)/(16*(long int)USARTBAUD)); //calculate baud rate and set
+    USART1.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_CHSIZE_8BIT_gc | \
+            USART_PMODE_EVEN_gc; //set asynch, 8bit frames, even parity
+    //note that parity mode may need to be changed for certain vendor devices
+    //Set tx pin as an output
+    PORTC.DIRSET = PIN0_bm; //port C pin 0 is txd for USART 0
+    // set interrupt RXCIE, DREIF is enabled in code before moving to the register
+    USART1.CTRLA = USART_RXCIE_bm;
+    //enable the receiver and transmitter
+    USART1.CTRLB = USART_RXEN_bm | USART_TXEN_bm;
+    //re-enable global interrupts
+    SREG |= ~CPU_I_bm;
+    //USART1 now initialized for 8bit, even parity, interrupt driven mode
+}
+
+void USART2_init(void)
+{
+    //start by disabling global interrupts (if they are enabled)
+    SREG &= ~CPU_I_bm; //set interrupt enable to zero to disable global interrupts
+    //Calculate baud rate
+    USART2.BAUD = ((64 * (long int)CPU_F)/(16*(long int)USARTBAUD)); //calculate baud rate and set
+    USART2.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_CHSIZE_8BIT_gc | \
+            USART_PMODE_EVEN_gc; //set asynch, 8bit frames, even parity
+    //note that parity mode may need to be changed for certain vendor devices
+    //Set tx pin as an output
+    PORTF.DIRSET = PIN0_bm; //port F pin 0 is txd for USART 0
+    // set interrupt RXCIE, DREIF is enabled in code before moving to the register
+    USART2.CTRLA = USART_RXCIE_bm;
+    //enable the receiver and transmitter
+    USART2.CTRLB = USART_RXEN_bm | USART_TXEN_bm;
+    //re-enable global interrupts
+    SREG |= ~CPU_I_bm;
+    //USART1 now initialized for 8bit, even parity, interrupt driven mode
+}
